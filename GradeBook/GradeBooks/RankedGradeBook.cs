f203b6dd557403c30228b9e94939b326834c9c2c
krishnavamshi24 <krishnavamshi24@live.com>
2019-05-16 23:32:34 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -37,27 +37,24 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if(Students.Count >= 5)
-            { 
-            base.CalculateStatistics();
-            }
-            else
+            if(Students.Count< 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+               
+                return;
             }
+            base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
-            if(Students.Count >=5)
+            if(Students.Count <5)
                     {
-                base.CalculateStudentStatistics(name);
-            }
-            else
-            {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
-         
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
